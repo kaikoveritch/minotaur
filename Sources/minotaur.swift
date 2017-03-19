860577d9f88125ec3433680bd35a51b799280532
kaikoveritch <kaiko.goren@gmail.com>
2017-03-19 21:14:53 +0100

    Added and verified doors, entrance, exit, minotaur and path functions. Commented out some functions and tests for testing purpose.

--- Sources/minotaur.swift
+++ Sources/minotaur.swift
@@ -28,6 +28,20 @@ struct Position : Equatable, CustomStringConvertible {
 
 }
 
+struct Path : Equatable {
+    let start : Term
+    let end : Term
+
+   //  var description: String {
+   //      return "\(self.x):\(self.y)"
+   //  }
+
+    static func ==(lhs: Path, rhs: Path) -> Bool {
+      return (lhs.start as! Position) == (rhs.start as! Position) && (lhs.end as! Position) == (rhs.end as! Position)
+    }
+
+}
+
 
 // rooms are numbered:
 // x:1,y:1 ... x:n,y:1
@@ -37,30 +51,69 @@ func room (_ x: Int, _ y: Int) -> Term {
   return Value (Position (x: x, y: y))
 }
 
+func newPath(_ start: Term, _ end: Term) -> Term {
+   return Value(Path(start: start, end: end))
+}
+
 func doors (from: Term, to: Term) -> Goal {
-    // TODO
+    return  (from === room(1,2) && to === room(1,1)) ||
+            (from === room(1,2) && to === room(2,2)) ||
+            (from === room(1,3) && to === room(1,2)) ||
+            (from === room(1,4) && to === room(1,3)) ||
+            (from === room(2,1) && to === room(1,1)) ||
+            (from === room(2,2) && to === room(3,2)) ||
+            (from === room(2,3) && to === room(2,2)) ||
+            (from === room(2,3) && to === room(1,3)) ||
+            (from === room(2,4) && to === room(2,3)) ||
+            (from === room(3,1) && to === room(2,1)) ||
+            (from === room(3,2) && to === room(3,3)) ||
+            (from === room(3,2) && to === room(4,2)) ||
+            (from === room(3,4) && to === room(2,4)) ||
+            (from === room(3,4) && to === room(3,3)) ||
+            (from === room(4,1) && to === room(3,1)) ||
+            (from === room(4,2) && to === room(4,1)) ||
+            (from === room(4,2) && to === room(4,3)) ||
+            (from === room(4,4) && to === room(3,4))
 }
 
 func entrance (location: Term) -> Goal {
-    // TODO
+    return  (location === room(1,4)) || (location === room(4,4))
 }
 
 func exit (location: Term) -> Goal {
-    // TODO
+    return  (location === room(1,1)) || (location === room(4,3))
 }
 
 func minotaur (location: Term) -> Goal {
-    // TODO
+    return (location === room(3,2))
+}
+
+func path_rec (from: Term, to: Term, through: Term, subs: Term) -> Goal {
+   return   (doors(from: from, to: to) && (List.cons(to, subs) === through)) ||
+            (
+               fresh{x in
+                  doors(from: from, to: x) &&
+                  path_rec(from: x, to: to, through: through, subs: List.cons(x, subs))
+               }
+            )
 }
 
 func path (from: Term, to: Term, through: Term) -> Goal {
-    // TODO
+   let subs = List.cons(from, List.empty)
+    return  path_rec(from: from, to: to, through: through, subs: subs)
 }
 
-func battery (through: Term, level: Term) -> Goal {
-    // TODO
-}
-
-func winning (through: Term, level: Term) -> Goal {
-    // TODO
-}
+// func battery (through: Term, level: Term) -> Goal {
+//     return  (level === succ(succ(zero)) && doors(from: (through as! Path).start, to: (through as! Path).end)) ||
+//             (
+//                fresh{x in fresh{y in
+//                   (succ(x) === level) && doors(from: (through as! Path).start, to: y) &&
+//                   battery(through: newPath(y, (through as! Path).end), level: x)
+//                }}
+//             )
+// }
+//
+// func winning (through: Term, level: Term) -> Goal {
+//     return  entrance(location: (through as! Path).start) && exit(location: (through as! Path).end) &&
+//             battery(through: through, level: level)
+// }
